415c311a11f1f0fe5bf6770801ca686bc0bf57aa
GONCHAROV-DV <botwy@mail.ru>
2019-05-29 22:32:44 +0300

    Initial Commit

---
+++ ContactsExample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ContactsExample
+//
+//  Created by Admin on 29.05.2019.
+//  Copyright © 2019 goncharov denis. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+
+}
+
