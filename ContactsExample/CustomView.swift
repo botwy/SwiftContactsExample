6c54c0a3e0897c21f44960a443705de6fb5c6251
GONCHAROV-DV <botwy@mail.ru>
2019-06-02 23:59:48 +0300

    contacts card: gesture recognizers, segue to call and mail app

---
+++ ContactsExample/CustomView.swift
@@ -0,0 +1,27 @@
+//
+//  CustomView.swift
+//  ContactsExample
+//
+//  Created by Admin on 29.05.2019.
+//  Copyright © 2019 goncharov denis. All rights reserved.
+//
+
+import UIKit
+
+class CustomView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+  override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+    
+  }
+  
+  override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
+    
+  }
+}
